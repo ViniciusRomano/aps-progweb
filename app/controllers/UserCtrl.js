ec99223e96d44de92b56219bad9568ca1e31b086
romano <romano@alunos.utfpr.edu.br>
2017-11-29 12:51:48 -0200

    aps3

--- app/controllers/UserCtrl.js
+++ app/controllers/UserCtrl.js
@@ -1,21 +1,7 @@
 function UserCtrl(app, mongoose) {
     var model = app.models.LoginModel;
     this.render = function (req, res, next) {
-        var imageModel = app.models.ImageModel;
-        var connection = new app.databases.mongoConnection();
-        imageModel.find({})
-            .then(
-            function success(images) {
-                res.render('home/index', { session: req.session, images: images, error: null });
-            }, function error(err) {
-                res.render('home/index', { session: req.session, images: images, error: err });
-                connection.close();
-            }
-            )
-            .catch(function (error) {
-                res.render('home/index', { session: req.session, images: images, error: error });
-                connection.close();
-            });
+        res.render('home/index', { session: req.session });
     }
 }
 
--- app/routes/home.route.js
+++ app/routes/home.route.js
@@ -1,8 +1,21 @@
 var mongoose = require('mongoose');
+var multer = require('multer')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'app/public/uploads')
+    },
+    filename: function (req, file, cb) {
+        var ext = file.originalname.substr(file.originalname.lastIndexOf('.') + 1);
+        cb(null, file.fieldname + '-' + Date.now() + '.' + ext);
+    }
+})
+var upload = multer({
+    storage: storage
+})
 
 module.exports = function (app) {
     var UserCtrl = new app.controllers.UserCtrl(app, mongoose);
-    app.get('/', function (req, res, next) {
+    app.get('/', upload.single('myimage'), function (req, res, next) {
         UserCtrl.render(req, res, next);
     });
 }
